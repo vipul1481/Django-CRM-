c42d95efb843c0d11f80db3293c833b025f18876
Vipul Goyal <vipulgoyal1481@gmail.com>
2023-07-17 01:18:04 +0530

    records_on_website

--- website/models.py
+++ website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+          
 # Create your models here.
+# database models do not depends upon which database you are using which database you are using
+# it will create it for you
+# after creating this u have to migrate it to your database
+# which can be done using terminal window 
+# step 1 makemigrations
+#step 2 migrate
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name =  models.CharField(max_length=50)
+    email =  models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address =  models.CharField(max_length=100)
+    city =  models.CharField(max_length=50)
+    state =  models.CharField(max_length=50)
+    zipcode =  models.CharField(max_length=20)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
+
+	
--- website/views.py
+++ website/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     # check to see if login in
     if (request.method == 'POST'):
         username = request.POST['username']
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request,"There was an error Logging In, Try Again")
             return redirect('home')
     else:    
-           return render(request,'home.html',{})
+           return render(request,'home.html',{'records':records})
 
 
 def logout_user(request):
