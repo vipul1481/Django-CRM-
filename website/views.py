6e9319d0a8732ccd2521aaa812311c21acc72af6
Vipul Goyal <vipulgoyal1481@gmail.com>
2023-07-17 00:25:33 +0530

    login_problem_solved

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     # check to see if login in
@@ -27,4 +27,22 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-     return render(request,'register.html',{})
+     if request.method == 'POST':
+          form =SignUpForm(request.POST)
+          if form.is_valid():
+            form.save()
+
+            # Authenticate and login 
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+     else:
+           form = SignUpForm()
+           return render(request, 'register.html', {'form':form})
+
+     return render(request, 'register.html', {'form':form})
+
+     
